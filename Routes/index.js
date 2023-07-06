78e467dd1fb2c607792388323daf25f9c3697da8
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-06 23:06:56 +0900

    npm install joi

--- Routes/index.js
+++ Routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express.Router();
 
-const { Posts } = require('../models');
+const { Post } = require('../models');
 
 //all read
 app.get('/', async (req, res) => {
   try {
-    res.status(201).json(await Posts.findAll());
+    res.status(201).json(await Post.findAll());
   } catch (err) {
     console.error(err);
     return res.status(400).json({ message: '전체 데이터를 불러오는데 실패하였습니다.' });
